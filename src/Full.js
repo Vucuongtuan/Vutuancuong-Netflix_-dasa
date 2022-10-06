88f39b5355ef243f261f25abb4a798002dfaa19a
Vucuongtuan <114804308+Vucuongtuan@users.noreply.github.com>
2022-10-06 15:57:41 +0700

    Add files via upload

---
+++ src/Full.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+html, body{
+    -webkit-font-smoothing:antialiased;
+    -moz-osx-font-smoothing:grayscale;
+    background-color:black;
+    color:#333333;
+    font-size:18px;
+}
+`;
